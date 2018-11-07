402a5d9a66676b768495130cb8c5879375a5ecf0
MarceloBD <marcelodianib@gmail.com>
2018-11-06 23:16:21 -0200

    Done

--- exVue/vue.js
+++ exVue/vue.js
@@ -1,7 +1,8 @@
 var comp = Vue.component('component-add', {
+	props: ['items'],
 	methods:{
 		add:function(){
-			this.items.push({text:this.items.text});
+			this.$parent.items.push({text:this.$parent.items.text});
 		}
 	},
 	template: '<input type="submit" name="send" value="Adicionar" v-on:click="add"/>'
