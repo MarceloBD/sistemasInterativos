bb3ccfafd607d60aa599736e608019d53b651bf0
MarceloBD <marcelodianib@gmail.com>
2018-11-17 13:43:54 -0200

    Separating in two pages

---
+++ ex3/README.md
@@ -0,0 +1,14 @@
+Para a leitura da página é necessário a abertura do código "main.html" por um navegador que de suporte ao HTML5.
+
+No desenvolvimento foi-se utilizado 
+
+Mozilla Firefox 62.0
+
+e
+
+SO
+Distributor ID:	Ubuntu
+Description:	Ubuntu 18.04.1 LTS
+Release:	18.04
+Codename:	bionic
+
---
+++ ex3/companyRegister.js
@@ -0,0 +1,105 @@
+var companyCount = 0
+
+function setFirstJobNotRequired() {
+	$('#fjob').prop('required', false)
+}
+
+
+function addNewCompany() {
+	setFirstJobNotRequired();
+
+	companyCount++;
+	var div = document.getElementById("job");
+
+	var fieldset = document.createElement("fieldset");;
+	fieldset.setAttribute("style", "width:100%");
+
+	var legend = document.createElement("legend");
+	var legendText = document.createTextNode("Emprego "+companyCount);
+	legend.appendChild(legendText);
+	fieldset.appendChild(legend);
+
+	var node = document.createElement("input");
+	var text = document.createTextNode("Nome da empresa");
+	
+
+	node.setAttribute("type", "text");
+	node.setAttribute("sytle","padding-right: 60px")
+	node.setAttribute('style', 'width:220px;');	
+	node.setAttribute("required", "true");
+	node.setAttribute("name", "company_name");
+	fieldset.appendChild(text);
+	fieldset.appendChild(node);
+	
+	var space = document.createElement('span');
+	space.setAttribute('style', 'width:33px; display:inline-block');	
+	fieldset.appendChild(space);
+
+	node = document.createElement("input");
+	text = document.createTextNode("País");
+	node.setAttribute("required", "true")
+	node.setAttribute('style', 'width:230px;');
+		node.setAttribute("name", "company_country");
+	fieldset.appendChild(text);
+	fieldset.appendChild(node);
+	linebreak = document.createElement("br");
+	fieldset.appendChild(linebreak);
+
+	node = document.createElement("input");
+	text = document.createTextNode("Segmento (Por exemplo, Tecnologia e Informática)");	
+	node.setAttribute("required", "true")
+	node.setAttribute('style', 'width:230px;');
+		node.setAttribute("name", "area")
+	fieldset.appendChild(text);
+	fieldset.appendChild(node);
+	linebreak = document.createElement("br");
+	fieldset.appendChild(linebreak);
+
+	node = document.createElement("input");
+	text = document.createTextNode("Início");
+	node.setAttribute("required", "true")
+	node.setAttribute('style', 'width:230px;');
+	node.setAttribute("type", "date");
+		node.setAttribute("name", "start");
+	
+	fieldset.appendChild(text);
+	fieldset.appendChild(node);
+
+
+	node = document.createElement("input");
+	text = document.createTextNode("Término");
+	fieldset.appendChild(text);
+	fieldset.appendChild(node);
+	node.setAttribute('style', 'width:230px;');
+	node.setAttribute("type", "date");
+		node.setAttribute("name", "end");
+	linebreak = document.createElement("br");
+	fieldset.appendChild(linebreak);
+
+	node = document.createElement("input");
+	text = document.createTextNode("Descrição (Informação complementar das atividades desenvolvidas)");
+	node.setAttribute("style","width:519px");
+	node.setAttribute("required", "true");
+		node.setAttribute("name", "description");
+	fieldset.appendChild(text);
+	fieldset.appendChild(node);
+	linebreak = document.createElement("br");
+	fieldset.appendChild(linebreak);	
+
+	div.appendChild(fieldset);
+}
+
+
+function firstJob(){
+
+	is_set = $('#fjob').prop('checked');
+
+	if(is_set){
+		$('#addCompBut').prop('disabled', true);
+	} else{
+		$('#addCompBut').prop('disabled', false);
+	}
+
+
+}
+
