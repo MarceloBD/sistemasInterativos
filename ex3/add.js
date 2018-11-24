6cb746adcc3d836734445b8b952a81b4cf7df95e
MarceloBD <marcelodianib@gmail.com>
2018-11-24 16:42:40 -0200

    Props

--- ex3/add.js
+++ ex3/add.js
@@ -1,6 +1,7 @@
 
 var company = Vue.component('Company', {
-	  template: '<div id="job"><fieldset style="width:100%"><legend>Emprego </legend>Nome da empresa<input type="text" sytle="padding-right: 60px" style="width:220px;" required="true" name="company_name"><span style="width:33px; display:inline-block"></span>País<input required="true" style="width:230px;" name="company_country"><br>Segmento (Por exemplo, Tecnologia e Informática)<input required="true" style="width:230px;" name="area"><br>Início<input required="true" style="width:230px;" type="date" name="start">Término<input style="width:230px;" type="date" name="end"><br>Descrição (Informação complementar das atividades desenvolvidas)<input style="width:519px" required="true" name="description"><br></fieldset></div>'
+	props:['i'],
+	  template: '<div id="job"><fieldset style="width:100%"><legend>Emprego {{i}}</legend>Nome da empresa<input type="text" sytle="padding-right: 60px" style="width:220px;" required="true" name="company_name"><span style="width:33px; display:inline-block"></span>País<input required="true" style="width:230px;" name="company_country"><br>Segmento (Por exemplo, Tecnologia e Informática)<input required="true" style="width:230px;" name="area"><br>Início<input required="true" style="width:230px;" type="date" name="start">Término<input style="width:230px;" type="date" name="end"><br>Descrição (Informação complementar das atividades desenvolvidas)<input style="width:519px" required="true" name="description"><br></fieldset></div>'
 	});
 
 const but_comp = new Vue({
